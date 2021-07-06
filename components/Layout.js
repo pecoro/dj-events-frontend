c1d814e572a6a149bfaca35a9851195eee0b1be7
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2021-07-06 10:38:18 +0200

    15.getServerSideProps & getStaticProps

--- components/Layout.js
+++ components/Layout.js
@@ -9,11 +9,11 @@ export default function Layout({title,keywords,description,children}) {
     const router = useRouter()
     return (
         <div>
-            <head>
+            <Head>
                 <title>{title}</title>
                 <meta name="description" content={description}/>
                 <meta name="keywords" content={keywords}/>
-            </head>
+            </Head>
 
             <Header />
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
 
-export default function Home() {
+export default function HomePage({events}) {
+  // ここでのconsole.logはクライアントサイドなので、ブラウザのconsole.logに出力される
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to Show</h3>}
+
+      {events.map(evt => (
+        <h3 key={evt.id}>{evt.name}</h3>
+      ))}
     </Layout>
   )
+}
+
+export async function getStaticProps(){
+  //ここでのconsole.logはサーバーサイドなので、下のターミナルに出力される
+
+  // TypeError: Only absolute URLs are supported. This error happened while generating the page. Any console logs will be displayed in the terminal window.
+  // const res = await fetch('${API_URL}/api/events');
+  const res = await fetch(API_URL + '/api/events');
+  const events = await res.json();
+
+  return{
+    props:{events},
+    revalidate:1,
+  }
 }
