1b56d69527ad62999ba9cfd700425a401a9369c6
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2021-07-22 15:16:10 +0200

    24. Search Component

---
+++ components/Search.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import styles from '@/styles/Search.module.css'
+import {useRouter} from 'next/router'
+
+export default function Search() {
+    const [term,setTerm] = useState('')
+
+    const router = useRouter()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        router.push(`/events/search?term=${term}`)
+        setTerm('')
+    }
+
+    return (
+        <div lassName={styles.search}>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text" value={term} 
+                onChange={(e) => setTerm(e.target.value)} 
+                placeholder="Search Events"
+                />
+            </form>
+        </div>
+    )
+}
