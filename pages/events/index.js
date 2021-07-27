a2f68745fe024a76a11a31e06b6c73267c7d7c21
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2021-07-27 15:15:01 +0200

    32. Events Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
-import { API_URL } from '@/config/index'
+import Pagenation from '@/components/Pagenation'
+import { API_URL, PER_PAGE} from '@/config/index'
 
-
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
     // ここでのconsole.logはクライアントサイドなので、ブラウザのconsole.logに出力される
     // console.log('fronend' + events);
-    console.log('frontend' + JSON.stringify(events));
+    // console.log('frontend' + JSON.stringify(events));
     return (
         <Layout>
             <h1>Events</h1>
@@ -15,21 +15,35 @@ export default function EventsPage({ events }) {
             {events.map((evt) => (
                 <EventItem key={evt.id} evt={evt} />
             ))}
+
+            <Pagenation page={page} total={total}/>
         </Layout>
     )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({query:{page=1}}) {
     //ここでのconsole.logはサーバーサイドなので、下のターミナルに出力される
 
-    const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-    const events = await res.json();
+    //Calculate start page
+    const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+
+    // Fetch total/count
+    const totalRes = await fetch(
+        `${API_URL}/events/count`
+    );
+    const total = await totalRes.json();
+
+
+    // fetch events
+    const eventRes = await fetch(
+        `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+        );
+    const events = await eventRes.json();
 
     // console.log('backend' + events);
-    console.log('backend' + JSON.stringify(events));
+    // console.log('backend' + JSON.stringify(events));
 
     return {
-        props: { events },
-        revalidate: 1,
+        props: { events , page: +page, total},
     }
 }
