ee2ef302efcc38b9b31c479ca837a8230c5164e2
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2021-07-28 14:19:36 +0200

    36. Login & Get JWT

---
+++ pages/api/login.js
@@ -0,0 +1,36 @@
+import {API_URL} from '@/config/index'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req,res) => {
+    if(req.method === 'POST'){
+        const {identifier,password} = req.body
+
+        const strapiRes = await fetch(`${API_URL}/auth/local`,
+        {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                identifier,
+                password
+            })
+        })
+
+        const data = await strapiRes.json()
+        
+        if(strapiRes.ok){
+            // @todo - Set cookie
+            res.status(200).json({user:data.user})
+        }else{
+            res
+                .status(data.statusCode)
+                .json({message:data.message[0].messages[0].message})
+        }
+
+        res.status(200).json({})
+     } else {
+            res.setHeader('Allow',['POST'])
+            res.status(405).json({message:`Method ${req.method} not allowed`})
+    }
+}
