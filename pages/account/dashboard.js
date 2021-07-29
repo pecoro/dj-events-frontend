3789ce39047b6d77630c40999adab2d266a9c6eb
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2021-07-29 14:24:11 +0200

    46. Authenticated Edit, Delete & Image Upload

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,13 +1,33 @@
 import {parseCookies} from '@/helpers/index'
+import {useRouter} from 'next/router'
 import Layout from '@/components/Layout'
 import DashboardEvent from '@/components/DashboardEvent'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Dashboard.module.css'
 
-export default function DashboardPage({events}) {
-    const deleteEvent = (id) => {
-        console.log(id);
+export default function DashboardPage({events , token}) {
+
+    const router = useRouter()
+    
+    const deleteEvent = async (id) => {
+        if (confirm('Are you sure?')) {
+            const res = await fetch(`${API_URL}/events/${id}`, {
+                method: 'DELETE',
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+            })
+
+            const data = await res.json()
+
+            if (!res.ok) {
+                toast.error(data.message)
+            } else {
+                router.reload()
+            }
+        }
     }
+
     return (
         <Layout title='User Dashboard'>
             <div className={styles.dash}>
@@ -37,7 +57,8 @@ export async function getServerSideProps({req}){
 
     return{
         props:{
-            events
+            events,
+            token
         },
     }
 }
